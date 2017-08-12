a4bbda8eb91fd7b1775a45f378ff6812804ac5fb
abdulsamad aliyu <abdulsamadaliyu17@gmail.com>
2017-08-12 20:25:36 +0100

    added about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,22 @@ var React = require('react');
 
 var About = (props) => {
   return(
-    <h3>About Component</h3>
+    <div>
+      <h1 className = "text-center">About</h1>
+      <p>This is a weather application built on react by Abdulsamad Aliyu. I have built this just to cure my boredom</p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href = "https://facebook.github.io/react">React</a> - This was the javascript framework used.
+        </li>
+        <li>
+          <a href = "http://openweathermap.org">Open Weather Map</a> - A very useful API
+        </li>
+      </ul>
+    </div>
+
   );
 }
 
